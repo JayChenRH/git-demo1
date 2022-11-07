70f8533be3f28e163eef4fbd1f6683bca81bdd38
Jay <18856356976@163.com>
2022-11-07 13:49:23 +0800

    first commit

---
+++ src/main/java/com/chen/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.chen.git;/*
+  @author Jay Chen
+  
+  @ Create  
+    
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
