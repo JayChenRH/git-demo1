c8d4b37dffd8610a7b4f27d50f5ffc98238b0497
Jay <18856356976@163.com>
2022-11-07 14:01:17 +0800

    hot-fix commit

--- src/main/java/com/chen/git/GitTest.java
+++ src/main/java/com/chen/git/GitTest.java
@@ -8,5 +8,9 @@ package com.chen.git;/*
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+
     }
 }
